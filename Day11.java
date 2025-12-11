2ec8987f5dbded65b80e7bd669b93a8f3538617e
Jage Grimes <226263@andrews.esc18.net>
2025-12-11 11:15:28 -0600

    Day 11 part one done, fix part two

--- Day11.java
+++ Day11.java
@@ -13,8 +13,8 @@ public class Day11
             
             Scanner chop = new Scanner(txt);
             
-            partOne(chop);
-            //partTwo(chop);
+            //partOne(chop);
+            partTwo(chop);
             
             chop.close();
         }catch(Error e) {}
@@ -23,7 +23,6 @@ public class Day11
     public static void partOne(Scanner chop)
     {
         HashMap<String, ArrayList<String>> inOut = new HashMap<>();
-        HashMap<String, Boolean> tracked = new HashMap<>();
         
         while(chop.hasNextLine())
         {
@@ -46,16 +45,15 @@ public class Day11
         }
         
         int total = 0;
-        for(String key : inOut.keySet())
+        for(String str : inOut.get("you"))
         {
-            tracked.put(key, true);
-            recPartOne(inOut, tracked, inOut.get(key), key);
+            total += recPartOne(inOut, inOut.get(str), str);
         }
         
         System.out.println(total);
     }
     
-    public static int recPartOne(HashMap<String, ArrayList<String>> inOut, HashMap<String, Boolean> tracked, ArrayList<String> out, String in)
+    public static int recPartOne(HashMap<String, ArrayList<String>> inOut, ArrayList<String> out, String in)
     {
         if(in.equals("out"))
         {
@@ -64,17 +62,68 @@ public class Day11
         int sum = 0;
         for(String str : out)
         {
-            if(tracked.containsKey(str))
-            {
-                continue;
-            }
-            sum += recPartOne(inOut, tracked, inOut.get(str), str);
+            sum += recPartOne(inOut, inOut.get(str), str);
         }
         
         return sum;
     }
     
-    
+    public static void partTwo(Scanner chop)
+    {
+        HashMap<String, ArrayList<String>> inOut = new HashMap<>();
+
+        while(chop.hasNextLine())
+        {
+            String line = chop.nextLine();
+            
+            Scanner chopper = new Scanner(line);
+            
+            String in = chopper.next();
+            in = in.substring(0, in.length() - 1);
+            
+            ArrayList<String> out = new ArrayList<>();
+            
+            while(chopper.hasNext())
+            {
+                out.add(chopper.next());
+            }
+            
+            inOut.put(in, out);
+            chopper.close();
+        }
+        int total = 0;
+        for(String str : inOut.get("svr"))
+        {
+            total += recPartTwo(inOut, inOut.get(str), str, false, false);
+        }
+
+        System.out.println(total);
+    }
+
+    public static int recPartTwo(HashMap<String, ArrayList<String>> inOut, ArrayList<String> out, String in, boolean dac, boolean fft)
+    {
+        if(in.equals("out") && dac && fft)
+        {
+            return 1;
+        }
+
+        int sum = 0;
+        for(String str : out)
+        {
+            if(str.equals("dac"))
+            {
+                dac = true;
+            }else if(str.equals("fft"))
+            {
+                fft = true;
+            }
+
+            System.out.println(str);
+            
+            sum += recPartTwo(inOut, inOut.get(str), str, dac, fft);
+        }
+        return sum;
+    }
     
     
     
